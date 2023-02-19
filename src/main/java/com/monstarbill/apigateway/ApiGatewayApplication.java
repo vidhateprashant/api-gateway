a81fff5d1e8efb65d0d44303c4c4a1f1f7cb1583
vidhateprashant <vidhate.prashant98@gmail.com>
2023-02-19 12:38:20 +0530

    cors

--- src/main/java/com/monstarbill/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/monstarbill/apigateway/ApiGatewayApplication.java
@@ -2,12 +2,27 @@ package com.monstarbill.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+	
+	@Bean
+	public WebFluxConfigurer corsMappingConfigurer() {
+	    return new WebFluxConfigurer() {
+	        @Override
+	        public void addCorsMappings(CorsRegistry registry) {
+	            registry.addMapping("/**")
+				.allowedOrigins("http://43.205.33.156:8080")
+				.allowedOrigins("http://43.205.33.156:8082");
+	        }
+	    };
 	}
 
 }

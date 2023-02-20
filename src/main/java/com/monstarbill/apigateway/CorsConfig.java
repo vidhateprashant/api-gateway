253ae42263427d121ab28d25d71c33267b0688f3
vidhateprashant <vidhate.prashant98@gmail.com>
2023-02-20 15:20:49 +0530

    cors

--- src/main/java/com/monstarbill/apigateway/CorsConfig.java
+++ src/main/java/com/monstarbill/apigateway/CorsConfig.java
@@ -1,38 +1,38 @@
-package com.monstarbill.apigateway;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-
-@Configuration
-public class CorsConfig {
-
-	@Bean
-	public CorsWebFilter corsFilter() {
-		return new CorsWebFilter(corsConfigurationSource());
-	}
-
-	@Bean
-	public CorsConfigurationSource corsConfigurationSource() {
-		System.out.println("=========== API Gateway : CorsConfig : corsConfigurationSource ");
-		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
-//		config.addAllowedMethod("*");
-		config.addAllowedMethod(HttpMethod.PUT);
-		config.addAllowedMethod(HttpMethod.DELETE);
-		config.addAllowedMethod(HttpMethod.GET);
-		config.addAllowedMethod(HttpMethod.OPTIONS);
-		config.addAllowedMethod(HttpMethod.POST);
-		config.addAllowedHeader("*");
-		config.addAllowedOrigin("*");
-		config.setMaxAge(99999L);
-
-		source.registerCorsConfiguration("/**", config);
-		return source;
-	}
-
-}
+//package com.monstarbill.apigateway;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.reactive.CorsConfigurationSource;
+//import org.springframework.web.cors.reactive.CorsWebFilter;
+//import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+//
+//@Configuration
+//public class CorsConfig {
+//
+//	@Bean
+//	public CorsWebFilter corsFilter() {
+//		return new CorsWebFilter(corsConfigurationSource());
+//	}
+//
+//	@Bean
+//	public CorsConfigurationSource corsConfigurationSource() {
+//		System.out.println("=========== API Gateway : CorsConfig : corsConfigurationSource ");
+//		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//		CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+////		config.addAllowedMethod("*");
+//		config.addAllowedMethod(HttpMethod.PUT);
+//		config.addAllowedMethod(HttpMethod.DELETE);
+//		config.addAllowedMethod(HttpMethod.GET);
+//		config.addAllowedMethod(HttpMethod.OPTIONS);
+//		config.addAllowedMethod(HttpMethod.POST);
+//		config.addAllowedHeader("*");
+//		config.addAllowedOrigin("*");
+//		config.setMaxAge(99999L);
+//
+//		source.registerCorsConfiguration("/**", config);
+//		return source;
+//	}
+//
+//}
